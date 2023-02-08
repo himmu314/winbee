d5a3564c7e69c7204dbc5dbb879a2979671725d2
Himanshu Gupta <gupta_h@India.XoriantCorp.com>
2023-02-08 15:59:32 +0530

    Added whatsapp link and changed addresses

---
+++ Animation/main.js
@@ -0,0 +1,18 @@
+
+(function() {
+  "use strict";
+
+  
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()
